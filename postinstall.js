b1c30d13e9b3978f43aeef551bf8af00d5b38bf1
shuisheng <2274751790@qq.com>
2022-12-01 21:45:50 +0800

    feat: 添加postinstall

---
+++ postinstall.js
@@ -0,0 +1,19 @@
+console.log();
+console.log(
+  '\033[0;37;44m  INFO  \033[0m',
+  '\033[0;;34m ' +
+    `Thank you for using billd-html-webpack-plugin ( https://github.com/galaxy-s10/billd-html-webpack-plugin ) !` +
+    ' \033[0m'
+);
+console.log();
+console.log(
+  '\033[0;37;44m Author \033[0m',
+  '\033[0;;34m ' + `https://github.com/galaxy-s10` + ' \033[0m'
+);
+console.log();
+
+console.log(
+  '\033[0;37;44m  BLOG  \033[0m',
+  '\033[0;;34m ' + `https://www.hsslive.cn` + ' \033[0m'
+);
+console.log();
